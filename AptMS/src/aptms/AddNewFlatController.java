97b4535e1c1f5decfd4a32733f3d3ce2d1f87d97
Shabbir <smzaman404@gmail.com>
2022-03-07 20:49:20 +0600

    addNewFlat updated

--- AptMS/src/aptms/AddNewFlatController.java
+++ AptMS/src/aptms/AddNewFlatController.java
@@ -6,7 +6,13 @@ package aptms;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -15,12 +21,72 @@ import javafx.fxml.Initializable;
  */
 public class AddNewFlatController implements Initializable {
 
+    @FXML
+    private TextField flatID_tf;
+    @FXML
+    private MenuButton vacancy_menubtn;
+    @FXML
+    private TextField rent_tf;
+    @FXML
+    private Button Save;
+    @FXML
+    private MenuButton gas_menubtn;
+    @FXML
+    private MenuItem gasYes_mi;
+    @FXML
+    private MenuItem gasNo_mi;
+    @FXML
+    private MenuButton electricity_menubtn;
+    @FXML
+    private MenuItem elecYes_mi;
+    @FXML
+    private MenuItem elecNo_mi;
+    @FXML
+    private MenuButton lift_menubtn;
+    @FXML
+    private MenuItem liftYes_mi;
+    @FXML
+    private MenuItem liftNo_mi;
+    @FXML
+    private MenuButton generator_menubtn;
+    @FXML
+    private MenuItem genYes_mi;
+    @FXML
+    private MenuItem genNo_mi;
+    @FXML
+    private TextField area_tf;
+    @FXML
+    private TextField bed_tf;
+    @FXML
+    private TextField bath_tf;
+    @FXML
+    private TextField level_tf;
+    @FXML
+    private TextField flatNo_tf;
+    @FXML
+    private TextField houseNo_tf;
+    @FXML
+    private TextField road_tf;
+    @FXML
+    private TextField block_tf;
+    @FXML
+    private TextField thana_tf;
+    @FXML
+    private TextField zilla_tf;
+    @FXML
+    private TextField division_tf;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        
     }    
+
+    @FXML
+    private void onClickUpdate_btn(ActionEvent event) {
+    }
     
 }
