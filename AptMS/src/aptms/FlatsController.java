e3e11ccb9349763c03df26d92172332fe7e17713
Shabbir <smzaman404@gmail.com>
2022-03-07 20:50:21 +0600

    flats show details

--- AptMS/src/aptms/FlatsController.java
+++ AptMS/src/aptms/FlatsController.java
@@ -4,16 +4,26 @@
  */
 package aptms;
 
+import aptms.utils.Bundle;
 import aptms.utils.TableLoader;
+import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TableView;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -38,6 +48,8 @@ public class FlatsController implements Initializable {
     TableLoader tableLoader = new TableLoader();
     @FXML
     private TableView<?> flats_tv;
+    @FXML
+    private Button gotoDetails_btn;
     /**
      * Initializes the controller class.
      */
@@ -52,5 +64,30 @@ public class FlatsController implements Initializable {
             Logger.getLogger(FlatsController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }    
+
+    @FXML
+    private void onClickGotoDetails_btn(ActionEvent event) throws IOException {
+        //System.out.println(coursesTable.getSelectionModel().getSelectedItem().toString());
+
+        Object s = flats_tv.getSelectionModel().getSelectedItems().get(0);
+
+        System.out.println(s.toString().split(", ")[1].substring(1));
+
+        String flatID = s.toString().split(", ")[0].substring(1); //got the 1st column of selected row -> first col = course id
+        System.out.println("flat id : " + flatID);
+        
+        Bundle.UID = flatID;
+        
+        final Stage dialog = new Stage();
+        dialog.initModality(Modality.APPLICATION_MODAL);
+        dialog.initOwner((Stage) ((Node) event.getSource()).getScene().getWindow());
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("FlatDetails.fxml"));
+
+        Parent rootPane = (Parent) loader.load();
+        FlatDetailsController controller = loader.getController();
+        Scene dialogScene = new Scene(rootPane);
+        dialog.setScene(dialogScene);
+        dialog.show();
+    }
     
 }
