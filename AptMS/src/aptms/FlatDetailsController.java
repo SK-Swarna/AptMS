92d1b50853eb8ac56589444dc66873bdf694edc2
Shabbir <smzaman404@gmail.com>
2022-03-08 03:14:12 +0600

    populated page with data

--- AptMS/src/aptms/FlatDetailsController.java
+++ AptMS/src/aptms/FlatDetailsController.java
@@ -8,6 +8,7 @@ import aptms.utils.Bundle;
 import aptms.utils.DBConnect;
 import aptms.utils.TableLoader;
 import java.net.URL;
+import java.sql.ResultSet;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -44,7 +45,7 @@ public class FlatDetailsController implements Initializable {
     @FXML
     private Label lift_label;
     @FXML
-    private Label generator_tf;
+    private Label generator_label;
     @FXML
     private Label sft_label;
     @FXML
@@ -64,9 +65,57 @@ public class FlatDetailsController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+//        // TODO
+//        Object s = flats_tv.getSelectionModel().getSelectedItems().get(0);
+//
+//        System.out.println(s.toString().split(", ")[1].substring(1));
+//
+//        String flatID = s.toString().split(", ")[0].substring(1); //got the 1st column of selected row -> first col = course id
+//        System.out.println("flat id : " + flatID);
+        
+        try{
+            DBConnect dbcon = new DBConnect();
+            dbcon.connectToDB();
+            
+            ResultSet flats = dbcon.queryToDB("select * from Flats where FlatID = " + Bundle.UID),
+                       fullAddress = dbcon.queryToDB("select * from FullAddress where FlatID = " + Bundle.UID),
+                       flatServices = dbcon.queryToDB("select * from FlatServices where FlatID = " + Bundle.UID),
+                       flatDetails = dbcon.queryToDB("select * from FlatDetails where FlatID = " + Bundle.UID);
+            flats.next();
+            fullAddress.next();
+            flatServices.next();
+            flatDetails.next();
+            System.out.println(flats.getString("vacancy_st") + " ::: " + String.valueOf(flatDetails.getDouble("Area")));
+            
+            vacancyStatus_label.setText(flats.getString("vacancy_st"));
+            rent_label.setText(String.valueOf(flats.getDouble("MonthlyRent")));
+            
+            bath_label.setText(String.valueOf(flatDetails.getInt("Bath")));
+            sft_label.setText(String.valueOf(flatDetails.getDouble("Area")));
+            bed_label.setText(String.valueOf(flatDetails.getInt("Bed")));
+            level_label.setText(":(");
+            gas_label.setText(flatServices.getString("Gas"));
+            electricity_label.setText(flatServices.getString("Electricity"));
+            lift_label.setText(flatServices.getString("lift"));
+            generator_label.setText(flatServices.getString("Generator"));
+            address_label.setText("wait");
+            
+            
+            
+        }
+        catch(Exception e) {
+            System.out.println("uh oh: " + e);
+        }
+        
+        
+        
+        
+        
         flatID_label.setText(Bundle.UID);
         System.out.println(Bundle.UID);
+        
+        
+        
         
     }    
 
@@ -82,9 +131,12 @@ public class FlatDetailsController implements Initializable {
            DBConnect dbcon = new DBConnect();
            dbcon.connectToDB();
            dbcon.insertDataToDB(qry);
+           
+           if(num) rent_label.setText(editProperties_tf.getText());
+           else vacancyStatus_label.setText(editProperties_tf.getText());
        }
        catch(Exception e) {
-           System.out.println("uh - oh");
+           System.out.println(":(");
        }
     }
 
